5635d2de6757f2645b3db84c51ff4727aa945dec
Decap <overu2soon@gmail.com>
2022-01-16 20:00:58 +0800

    first

---
+++ src/main/java/com/atbleu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atbleu.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author Decap
+ * @BelongsProject: IntelliJ IDEA
+ * @BelongsPackage: com.atbleu.git
+ * @create 2022-01-2022/1/1619:54
+ * @desc
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("idea git1");
+    }
+}
